b06e6d581d3b0094db2832ed8cea02900706d5e1
Seapoe1809 <129666906+seapoe1809@users.noreply.github.com>
2023-09-20 04:51:34 -0400

    Add files via upload

--- darna_launch.sh
+++ darna_launch.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #!/bin/bash
 cd Health_server  # Change to the directory containing the 'server' folder
-source darnav/bin/activate  # Activate the virtual environment
+source darnavenv/bin/activate  # Activate the virtual environment
 gunicorn -w 2 -b 0.0.0.0:3001 darna:app
 
