3c9d8fafd2aa07b279c7ca242e2f0dc5ff0d565f
Seapoe1809 <129666906+seapoe1809@users.noreply.github.com>
2023-09-20 05:37:14 -0400

    Add files via upload

--- darna_launch.sh
+++ darna_launch.sh
@@ -2,5 +2,5 @@
 #!/bin/bash
 cd Health_server  # Change to the directory containing the 'server' folder
 source darnavenv/bin/activate  # Activate the virtual environment
-python3 darna.py
+nohup python3 darna.py &
 
