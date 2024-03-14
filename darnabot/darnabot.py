c00285da539f4fdb52f37569abedf6f92e4b16b3
Seapoe1809 <129666906+seapoe1809@users.noreply.github.com>
2024-03-14 04:53:59 -0400

    Add files via upload

--- darnabot/darnabot.py
+++ darnabot/darnabot.py
@@ -1,4 +1,21 @@
-#launching tinyllama chat and get it to say something nice
+#/* DARNA.HI
+# * Copyright (c) 2023 Seapoe1809   <https://github.com/seapoe1809>
+# * Copyright (c) 2023 pnmeka   <https://github.com/pnmeka>
+# * 
+# *
+# *   This program is free software: you can redistribute it and/or modify
+# *   it under the terms of the GNU General Public License as published by
+# *   the Free Software Foundation, either version 3 of the License, or
+# *   (at your option) any later version.
+# *
+# *   This program is distributed in the hope that it will be useful,
+# *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+# *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# *   GNU General Public License for more details.
+# *
+# *   You should have received a copy of the GNU General Public License
+# *   along with this program. If not, see <http://www.gnu.org/licenses/>.
+# */
 #modules torch, sentence transformers, gradio, json
 #uses chromaminer to chunk and embed and then uses function to extract relevant component
 import torch
